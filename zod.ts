6bfeb16c451ac3d24e9488908bfc6b82fde77a62
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-05-02 21:45:36 +0700

    ZOD

---
+++ zod.ts
@@ -0,0 +1,14 @@
+import { password } from "bun";
+import {z} from "zod";
+
+const loginValidation = z.object({
+    username: z.string().min(1).max(50).email(),
+    password: z.string().min(1).max(50)
+});
+
+const request = {
+    username: 'aldi@example.com',
+    password: 'rahasia'
+};
+
+loginValidation.parse(request);
